8e61531f03c7ff96f6416c4c0176a9bd98046d91
gmilsz-P50 <gmillarsaez@gmail.com>
2022-09-18 15:17:23 -0300

    correccion resumenes

--- resumen_temperaturas.py
+++ resumen_temperaturas.py
@@ -1,15 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[25]:
-
-
 import pandas as pd
 import os
 from tqdm import tqdm
-
-
-# In[21]:
 
 
 try:
@@ -18,16 +9,13 @@ except:
     None
 
 
-# In[26]:
-
-
 for estacion in tqdm(os.listdir('datosTemperatura')):
     archivos = sorted(os.listdir('datosTemperatura/'+estacion))
-    
+
     resumen = pd.read_csv('datosTemperatura/'+estacion+'/'+archivos[0])
-    for archivo in archivos[1:]:
+    for archivo in archivos:
         data_aux = pd.read_csv('datosTemperatura/'+estacion+'/'+archivo)
         resumen = pd.concat((resumen, data_aux))
-        
-    resumen.to_csv('resumenTemperatura/'+estacion+'.csv')
 
+    if len(resumen) > 1:
+    	resumen.to_csv('resumenTemperatura/'+estacion+'.csv', index=False)
